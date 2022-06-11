105f73b8b5f605a701d5f9688c83f728a91d6d44
juan esteban <juan.garciam1@udea.edu.co>
2022-06-11 18:09:59 -0500

    Se crea primera pantalla de la app, en portrait y landscape

--- Lab1-UI/src/main/java/co/com/udea/compumovil/gr02_20221/lab1/MainActivity.kt
+++ Lab1-UI/src/main/java/co/com/udea/compumovil/gr02_20221/lab1/MainActivity.kt
@@ -1,12 +1,44 @@
 package co.com.udea.compumovil.gr02_20221.lab1
 
+import android.app.Activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Spinner
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+
+        val grades = resources.getStringArray(R.array.grades)
+        val spinner: Spinner = findViewById(R.id.academicGrade)
+        val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, grades)
+        spinner.adapter = adapter
+
+        spinner.onItemSelectedListener = object :
+            AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(
+                parent: AdapterView<*>,
+                view: View,
+                position: Int,
+                id: Long
+            ) {
+                Toast.makeText(
+                    this@MainActivity,
+                    getString(R.string.selected_item) + " " +
+                            "" + grades[position], Toast.LENGTH_SHORT
+                ).show()
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>) {
+                // write code to perform some action
+            }
+        }
     }
 }
+
